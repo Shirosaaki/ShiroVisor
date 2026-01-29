cd2bfa35cf9bcfb6b823a1e31b6f3a36dd0985a7
shirosaaki <matheo.campanella@epitech.eu>
2026-01-29 10:32:56 +0100

    [+] read input

--- asm/payload.asm
+++ asm/payload.asm
@@ -9,52 +9,39 @@
 [ORG 0x0000]
 
 start:
-    ; Initialisation
     xor ax, ax
     mov ds, ax
-    mov es, ax
     mov ss, ax
-    mov sp, 0x7000    
+    mov sp, 0x7000
 
-    ; Affichage de test
-    mov ax, -32768    ; Le nombre min en 16-bit
-    call my_putnbr
+    mov si, prompt
+    call print_string
+
+loop_echo:
+    ; Lire un caractère (IN depuis le port 0x11)
+    in al, 0x11
     
-    mov al, 10        ; \n
-    out 0x10, al
-    mov al, 13        ; \r
-    out 0x10, al
+    ; Si c'est 'q', on arrête
+    cmp al, 'q'
+    je quit
 
-    mov ax, 12345
-    call my_putnbr
+    ; Afficher le caractère reçu (OUT vers le port 0x10)
+    out 0x10, al
+    jmp loop_echo
 
+quit:
+    mov si, bye
+    call print_string
     hlt
 
-my_putnbr:
-    pusha
-    cmp ax, 0
-    jge .is_pos
-    
-    push ax
-    mov al, '-'
+print_string:
+    lodsb
+    test al, al
+    jz .done
     out 0x10, al
-    pop ax
-    neg ax
-
-.is_pos:
-    mov bx, 10
-    xor cx, cx
-.extract:
-    xor dx, dx
-    div bx
-    add dl, '0'
-    push dx
-    inc cx
-    test ax, ax
-    jnz .extract
-.print:
-    pop ax
-    out 0x10, al
-    loop .print
-    popa
+    jmp print_string
+.done:
     ret
+
+prompt db "Tapez du texte ('q' pour quitter) : ", 0
+bye    db 10, 13, "Au revoir !", 10, 13, 0
